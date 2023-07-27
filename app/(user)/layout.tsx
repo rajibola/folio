a73f7906e7f2995d52e291403d59487281d44593
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2023-07-27 10:25:18 +0100

    feat: Added locomotive scroller

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/shared/header";
 import "../globals.css";
 import { Inter } from "next/font/google";
 import { PageTransition } from "@/shared/page-transition";
+import { LocomotiveScroller } from "@/shared/locomotive-scroller";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <LocomotiveScroller />
         <PageTransition />
         <Header />
         {children}
--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,11 +1,3 @@
-"use client";
-
-import { Header } from "@/shared/header";
-
 export default function Home() {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
+  return <div></div>;
 }
